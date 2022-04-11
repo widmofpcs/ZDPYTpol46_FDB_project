970c2e3d12b4e03d7b68c42426970f6fe9598ca9
dabri7 <Noweprogramowanie7>
2022-04-11 15:44:46 +0200

    CustomUser added

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class CustomUser(AbstractUser):
+    is_manager = models.BooleanField(default=False)
+    is_employee = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.username
