18a4b2c085c92bb47db0b26bce38ac5300d8661a
dabri7 <Noweprogramowanie7>
2022-04-13 19:35:35 +0200

    Task and RequestChangeTask

--- task/models.py
+++ task/models.py
@@ -1,13 +1,32 @@
-from django.db.models import F
 from django.db import models
 
-# Create your models here.
-from django.urls import reverse
+from accounts.models import CustomUser
 
 
 class Task(models.Model):
     name = models.CharField(max_length=128)
+    created_date = models.DateField(auto_now_add=True, editable=False)
+    date_modified = models.DateTimeField(auto_now=True)
     consumed_time = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=1)
     # id_customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     rate = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=1)
     total_cost = models.DecimalField(null=True, blank=True, max_digits=7, decimal_places=1)
+    is_active = models.BooleanField(default=True)
+
+
+class RequestChangeTask(models.Model):
+    STATUS = (
+        ('1', 'Waiting'),
+        ('2', 'Accepted'),
+        ('3', 'Denied'),
+    )
+    task_id = models.ForeignKey(Task, on_delete=models.CASCADE)
+    consumed_time = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=1)
+    rate = models.DecimalField(null=True, blank=True, max_digits=5, decimal_places=1)
+    is_active = models.BooleanField(default=True)
+    status = models.CharField(max_length=1, choices=STATUS, default='1')
+    requested_by = models.ForeignKey(CustomUser, on_delete=models.SET('Former employee'))
+    review_by = models.ForeignKey(CustomUser, on_delete=models.SET('Former manager'))
+
+
+
