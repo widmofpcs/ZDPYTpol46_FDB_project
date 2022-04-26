bf9726906235d49b673b065825328d525233e6dc
bart <bart.swiader@gmail.com>
2022-04-26 21:04:28 +0200

    zmerdzowalem se mastera na franxxa bo pracowalem na masterze

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from accounts.views import SignUpView, profile_update_view, profile_detail_view
+from accounts import views
+from accounts.views import SignUpView, profile_update_view, profile_detail_view, profile_list_view, UserCreateView
 
 app_name = 'accounts'
 
@@ -8,5 +9,8 @@ app_name = 'accounts'
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
     path('update/profile/', profile_update_view, name='update-profile'),
-    path('detail/profile/<int:pk>', profile_detail_view, name='detail-profile')
+    path('detail/profile/<int:pk>', profile_detail_view, name='detail-profile'),
+    path('list/', profile_list_view, name='list-profile'),
+    path('form/', UserCreateView.as_view(), name='form-profile'),
+    path('delete/<int:pk>/', views.UserDeleteView.as_view(), name='delete-profile'),
 ]
