871d9dba49499a3bc19da9ddb748d7a61c61d76a
bart <bart.swiader@gmail.com>
2022-04-30 21:39:09 +0200

    Account app: -password reset rev2 (form to reset is still default from django) but others done

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,9 +20,12 @@ urlpatterns = [
          auth_views.PasswordResetDoneView.as_view(template_name="user_registration/password_reset_sent.html"),
          name='password_reset_done'),
     path('reset/<uidb64>/<token>',
-         auth_views.PasswordResetConfirmView.as_view(template_name="user_registration/password_reset_sent.html"),
+         auth_views.PasswordResetConfirmView.as_view(template_name="user_registration/password_reset_form.html"),
          name='password_reset_confirm'),
     path('reset/done/',
          auth_views.PasswordResetCompleteView.as_view(template_name="user_registration/password_reset_done.html"),
          name='password_reset_complete')
+
+
+
 ]
